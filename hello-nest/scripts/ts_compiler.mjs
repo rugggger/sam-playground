453995d847eb652098700b199da51d58c60dc6ad
Yaron Bar <bar.yaron2018@gmail.com>
2024-09-01 11:22:39 +0300

    wip

---
+++ hello-nest/scripts/ts_compiler.mjs
@@ -0,0 +1,29 @@
+import { exec } from "child_process";
+import fs from 'fs/promises';
+import * as path from "path";
+async function compileTS(dir) {
+   const dirents = await fs.readdir(dir, { withFileTypes: true });
+   for (const dirent of dirents) {
+       const res = path.resolve(dir, dirent.name);
+       if (dirent.isDirectory() && !res.includes('node_modules')) {
+           await compileTS(res);
+       } else if (dirent.name.includes('.ts') && res.includes('Layer')) {
+           await execShellCommand(`npx tsc ${res}`);
+           console.log(dirent.name);
+       }
+   }
+}
+function execShellCommand(cmd) {
+   return new Promise((resolve, reject) => {
+       exec(cmd, (error, stdout, stderr) => {
+           if (error) {
+               console.warn(error);
+           }
+           resolve(stdout ? stdout : stderr);
+       });
+   });
+}
+(async () => {
+   const buildDirectory = process.argv[2] || '.aws-sam/build';
+   await compileTS(buildDirectory);
+})();
